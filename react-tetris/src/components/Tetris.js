6e1ab65e3a76275f5d7cc63579a0571adcedc52e
Janson <jansonchiu343@gmail.com>
2019-09-06 01:28:32 -0700

    fixed undefined errors in tetris.js

--- react-tetris/src/components/Tetris.js
+++ react-tetris/src/components/Tetris.js
@@ -20,41 +20,43 @@ const Tetris = () => {
     const [dropTime, setDropTime] = useState(null);
     const [gameOver, setGameOver] = useState(false);
 
-    const [player] = usePlayer();
+    const [player, updatePlayerPos, resetPlayer] = usePlayer();
     const [stage, setStage] = useStage(player);
 
     console.log('re-render');
 
     //move player function 
     const movePlayer = dir => { 
-
+      updatePlayerPos({ x:dir, y:0 });
     }
 
     const startGame = () => { 
-
+      //Reset everything 
+      setStage(createStage());
+      resetPlayer();
     }
 
     const drop = () => { 
-
+      updatePlayerPos({ x:0, y: 1, collided: false });
     }
 
     /** */
     const dropPlayer = () => { 
+      drop();
+
+    }
+
+    const move = ({ keyCode }) => { 
       //check if game over 
       if(!gameOver) { 
         if(keyCode === 37) { 
           movePlayer(-1);           //move player to the left 
         } else if (keyCode === 39) {
           movePlayer(1);
-        } else if( keycode === 40) { 
+        } else if( keyCode === 40) { 
           dropPlayer();
         }
       }
-
-    }
-
-    const move = ({ keyCode }) => { 
-
     }
 
     return ( 
@@ -71,7 +73,7 @@ const Tetris = () => {
             <Display text="Level" />
             </div>
           )}
-          <StartButton />
+          <StartButton callback={startGame}/>
         </aside>
         </StyledTetris>
       </StyledTetrisWrapper>
