77b43da60a7648d6cf660d4a2d595166b1122a50
Janson <jansonchiu343@gmail.com>
2019-09-09 21:35:27 -0700

    format and add spaces

--- react-tetris/src/hooks/usePlayer.js
+++ react-tetris/src/hooks/usePlayer.js
@@ -3,29 +3,28 @@ import { useState, useCallback } from 'react';
 import { TETROMINOS, randomTetromino } from '../tetrominos';
 import { STAGE_WIDTH } from '../gameHelpers';
 
-export const usePlayer = () => { 
-  //create state
+export const usePlayer = () => {
   const [player, setPlayer] = useState({
-    //pos is position of player 
     pos: { x: 0, y: 0 },
-    tetromino: TETROMINOS[0].shape, 
-    collided: false, 
+    tetromino: TETROMINOS[0].shape,
+    collided: false,
   });
 
-  const updatePlayerPos = ({ x, y, collided}) => { 
+  const updatePlayerPos = ({ x, y, collided }) => {
     setPlayer(prev => ({
-      ...prev, 
-      pos: { x: (prev.pos.x +=x), y: (prev.pos.y += y)},
+      ...prev,
+      pos: { x: (prev.pos.x += x), y: (prev.pos.y += y)},
       collided,
     }))
   }
 
-  const resetPlayer = useCallback(() => { 
+  const resetPlayer = useCallback(() => {
     setPlayer({
-      pos: { x: STAGE_WIDTH / 2-2, y: 0}, 
+      pos: { x: STAGE_WIDTH / 2 - 2, y: 0 },
       tetromino: randomTetromino().shape,
       collided: false,
     })
   }, [])
+
   return [player, updatePlayerPos, resetPlayer];
 }
